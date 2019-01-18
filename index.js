593673c507f32fa7d614cdaab893cc005944153b
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2019-01-17 19:29:26 -0500

    added search functionality so search results change results of API data

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ var request = require('request');
 
 app.set("view engine", 'ejs')
 
+app.get("/", function(req, res) {
+  res.render("search");
+})
+
 
 
 app.get('/results', function(req, res) {
+  var query = req.query.search
 
-  request('http://www.omdbapi.com/?s=chicago&apikey=thewdb', function(error, response, body){
+  var url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`;
+
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200) {
       var parsedData = JSON.parse(body)
         // res.send(parsedData['Search'][0].Title)
